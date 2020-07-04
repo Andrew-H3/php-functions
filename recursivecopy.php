ec2419b507d8b1f856e483ae1a4c976e09411f3a
Andrew Huebner <26437357+Andrew-H3@users.noreply.github.com>
2020-07-03 23:09:20 -0400

    Fixed GitHub formating error

--- recursivecopy.php
+++ recursivecopy.php
@@ -1,25 +1,24 @@
 <?php
-        function recursiveCopy($src,$tar) {
-                if (is_file($src)) {
-                        copy($src,$tar);
-                }
-                else {
-                        if (!file_exists($tar)) {
-                                mkdir($tar);
-                        }
-                        $dir = array_reverse(scandir($src));
-                        if (count($dir) > 2) {
-                                foreach ($dir as $x) {
-                                        if ($x == ".") {
-                                                continue;
-                                        }
-                                        elseif ($x == "..") {
-                                                continue;
-                                        }
-                                        echo "$src/$x\n";
-                                        recursiveCopy("$src/$x","$tar/$x");
-                                }
-                        }
-                }
-        }
+function recursiveCopy($src,$tar) {
+	if (is_file($src)) {
+		copy($src,$tar);
+	}
+	else {
+		if (!file_exists($tar)) {
+			mkdir($tar);
+		}
+		$dir = array_reverse(scandir($src));
+		if (count($dir) > 2) {
+			foreach ($dir as $x) {
+				if ($x == ".") {
+					continue;
+				}
+				elseif ($x == "..") {
+					continue;
+				}
+				recursiveCopy("$src/$x","$tar/$x");
+			}
+		}
+	}
+}
 ?>
